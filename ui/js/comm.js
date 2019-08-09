d8351ebbc364018e677a107792f8145de2a9b6dd
unknown <wangsw@sunallies.com>
2019-08-09 10:25:12 +0800

    上传时压缩图片

--- ui/js/comm.js
+++ ui/js/comm.js
@@ -977,6 +977,43 @@ var global = {
             zooms:[$scope.datas.map_zoom_min, $scope.datas.map_zoom_max_limit],
         });
 
+    },
+
+    compress: function (base64String, w, quality) {
+        var getMimeType = function (urlData) {
+            var arr = urlData.split(',');
+            var mime = arr[0].match(/:(.*?);/)[1];
+            // return mime.replace("image/", "");
+            return mime;
+        };
+        var newImage = new Image();
+        var imgWidth, imgHeight;
+ 
+        var promise = new Promise(resolve => newImage.onload = resolve);
+        newImage.src = base64String;
+        return promise.then(() => {
+            imgWidth = newImage.width;
+            imgHeight = newImage.height;
+            var canvas = document.createElement("canvas");
+            var ctx = canvas.getContext("2d");
+            if (Math.max(imgWidth, imgHeight) > w) {
+                if (imgWidth > imgHeight) {
+                    canvas.width = w;
+                    canvas.height = w * imgHeight / imgWidth;
+                } else {
+                    canvas.height = w;
+                    canvas.width = w * imgWidth / imgHeight;
+                }
+            } else {
+                canvas.width = imgWidth;
+                canvas.height = imgHeight;
+            }
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(newImage, 0, 0, canvas.width, canvas.height);
+            var base64 = canvas.toDataURL(getMimeType(base64String), quality);
+            console.log(base64);
+            return base64;
+        });
     }
 }
 
